ee2215ffc0dbc0b8c21ce009c667ef795a069731
cmlee <chukmunnlee@gmail.com>
2025-10-13 16:52:22 +0800

    day01 workshop

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,27 +11,28 @@ nvm install 22
 
 echo "Node version $(node -v)"
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 START="$(date)"
 
-pip3 install --upgrade pip
-pip3 install chromadb pypandoc
-pip3 install transformers datasets evaluate rouge_score loralib peft 
-pip3 install ipykernel ipywidgets 
-pip3 install langchain-community sentence-transformers unstructured
-pip3 install diffusers accelerate scipy safetensors
-pip3 install torch torchdata torchvision
-pip3 install smolagents openai
-pip3 install nbconvert[webpdf]
-pip3 huggingface_hub[hf_xet]
-
-pip3 install unstructured 
-pip3 install pandas networkx openpyxl
-pip3 install python-magic python-pptx
-pip3 install docx2txt docx msoffcrypto-tool
-pip3 install jq nltk
-pip3 install duckduckgo_search ddgs
-pip3 install pypdf pymupdf Pillow
-pip3 install fastmcp[cli]
+pip install --upgrade pip
+pip install chromadb pypandoc
+pip install transformers datasets evaluate rouge_score loralib peft 
+pip install ipykernel ipywidgets 
+pip install langchain-community sentence-transformers unstructured
+pip install diffusers accelerate scipy safetensors
+pip install torch torchdata torchvision
+pip install smolagents openai
+pip install nbconvert[webpdf]
+pip huggingface_hub[hf_xet]
+pip install unstructured 
+pip install pandas networkx openpyxl
+pip install python-magic python-pptx
+pip install docx2txt docx msoffcrypto-tool
+pip install jq nltk
+pip install duckduckgo_search ddgs
+pip install pypdf pymupdf Pillow
+pip install fastmcp[cli]
  
 echo "Preloads..."
 python3 .devcontainer/preload.py
