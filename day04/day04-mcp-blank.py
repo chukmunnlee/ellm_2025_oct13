5dbe1c2f8e38ef522e514b3e9ffab7eea47df09e
cmlee <chukmunnlee@gmail.com>
2025-10-16 14:42:59 +0800

    day04 PM demos

---
+++ day04/day04-mcp-blank.py
@@ -0,0 +1,40 @@
+import requests
+
+# TODO: Import fastmcp
+from mcp.server.fastmcp import FastMCP
+
+# Setup 
+# Open a terminal and type the following
+# source $HOME/.bashrc to set node environment
+
+# TODO: Create MCP server
+mcp = FastMCP('Crypto MCP')
+
+# Decorate the function to indicate that it is an tool
+@mcp.tool()
+def get_crypto_price(crypto = 'bitcoin') -> str:
+  """ 
+  Get the price of a crypto currency
+  Args:
+    crypto: crypto currency symbol eg. bitcoin, ethereum, dogecoin. The default is bitcoin
+  """
+  currency = 'sgd'
+  url = "https://api.coingecko.com/api/v3/simple/price"
+  params = { "ids": crypto.lower(), "vs_currencies": currency}
+  try:
+    resp = requests.get(url, params=params, timeout=10)
+    resp.raise_for_status()
+    data = resp.json()
+    price = data.get(crypto.lower(), {}).get(currency)
+    if price is not None:
+      return f"The price of {crypto} is {currency.upper()} {price}"
+
+    return f"Price for {crypto} is not found"
+  except Exception as e:
+    return f"Error fetching crypto: {e}"
+
+
+# TODO: Start the server
+if __name__ == "__name__":
+  mcp.run(host='0.0.0.0')
+
